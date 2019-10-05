e1dc737e0a3036551f802767285ad030215bc366
Ksenia <ksusha100@tut.by>
2019-10-06 02:18:33 +0300

    mistake was with mapping

--- NorthwindWebApiApp/MappingProfiles/OrderServiceMappingProfile.cs
+++ NorthwindWebApiApp/MappingProfiles/OrderServiceMappingProfile.cs
@@ -1,11 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
+using NorthwindWebApiApp.Models;
+using NorthwindWebApiApp.Services;
 
 namespace NorthwindWebApiApp.MappingProfiles
 {
-    public class OrderServiceMappingProfile
+    public class OrderServiceMappingProfile : Profile
     {
+        public OrderServiceMappingProfile()
+        {
+            Console.WriteLine("OrderServiceMappingProfile");
+
+            this.CreateMap<BriefOrderDescription, BriefOrderModel>();
+            this.CreateMap<BriefOrderVersion2Description, BriefOrderVersion2Model>();
+            this.CreateMap<FullOrderDescription, FullOrderModel>();
+        }
     }
 }
