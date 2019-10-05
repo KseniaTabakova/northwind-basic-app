45c9e00ff7335b22cb212ec37f510166549b6ef6
Ksenia <36692279+KseniaTabakova@users.noreply.github.com>
2019-10-06 02:20:26 +0300

    Update OrderServiceMappingProfile.cs

--- NorthwindWebApiApp/MappingProfiles/OrderServiceMappingProfile.cs
+++ NorthwindWebApiApp/MappingProfiles/OrderServiceMappingProfile.cs
@@ -5,7 +5,15 @@ using System.Threading.Tasks;
 
 namespace NorthwindWebApiApp.MappingProfiles
 {
-    public class OrderServiceMappingProfile
+    public class OrderServiceMappingProfile : Profile
     {
+        public OrderServiceMappingProfile()
+        {
+            Console.WriteLine("OrderServiceMappingProfile");
+
+            this.CreateMap<BriefOrderDescription, BriefOrderModel>();
+            this.CreateMap<BriefOrderVersion2Description, BriefOrderVersion2Model>();
+            this.CreateMap<FullOrderDescription, FullOrderModel>();
+        }    
     }
 }
